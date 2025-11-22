036421a471f043a8106f9d9d0d1f678da1cd81c9
codeenvo <codeenvo@gmail.com>
2025-11-22 03:44:31 +0000

    UI Update

--- frontend/src/components/Layout/ResearchLayout/ResearchLayout.jsx
+++ frontend/src/components/Layout/ResearchLayout/ResearchLayout.jsx
@@ -2,7 +2,7 @@ import ResearchNavbar from "./ResearchNavbar"
 
 const ResearchLayout = ({children, activeMenu}) => {
   return (
-    <div className="w-full items-center mt-4 sm:mt-20 text-black bg-cover min-h-screen my-12 sm:my-16">
+    <div className="w-full items-center mt-16 sm:mt-20 text-black bg-cover min-h-screen my-12 sm:my-16">
     {/* <div className=""> */}
         {/* <ResearchNavbar activeMenu={activeMenu} /> */}
         <div className="container mx-auto px-5 md:px-0 mt-0"> {children} </div>
